7cb9698c780728562d58f712fd8e505c529b9846
Akilah Snowden <akilahdsnowden@gmail.com>
2022-04-26 17:07:10 -0400

    commit tree 32e1f0bdecccae8456dbc23eaf192f30b82910a4

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+//should return a new array containing the first two drivers from the passed-in array:
+
+const returnFirstTwoDrivers = driversArray =>{
+    return driversArray.slice(0, 2)
+}
+
+const returnLastTwoDrivers = driverArray =>{
+        return driverArray.slice(-2)
+}
+
+// selectingDrivers  has the `returnFirstTwoDrivers` function to as its first element
+// has the `returnLastTwoDrivers` function to as its last element
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers] 
+
+//createFareMultiplier() returns a function
+function createFareMultiplier(int){
+    return function(fare){
+        return fare * int
+    }
+}
+//create a variable (fareDoubler)assigning function createFareMultiplier
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+    
+//function that takes two arguments- array of drivers and returnFirstTwoDrivers function
+
+function selectDifferentDrivers(arrayOfDrivers, returnsSomething){
+    return returnsSomething(arrayOfDrivers)
+}
